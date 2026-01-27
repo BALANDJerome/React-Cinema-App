bfd11d32ca7eb9f7ba86a7e363b7d97ebba69b40
BALAND Jérome <baland.jerome@gmail.com>
2026-01-27 12:02:23 +0100

    axios + debut  affichage Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,28 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ movie }) => {
+  console.log(movie);
+
   return (
     <div className="card">
-      <img src="" alt="" />
-      <h2></h2>
-      <h5></h5>
+      <img
+        src={"https://image.tmdb.org/t/p/original/" + movie.poster_path}
+        alt=""
+      />
+      <h2>{movie.title}</h2>
+      <h5>Sorti le : {movie.release_date}</h5>
       <h4>
-        <span></span>
+        {movie.vote_average}/10
+        <span>&#x2B50;</span>
       </h4>
       <ul>
-        <li></li>
+        <li>{movie.genre_ids}</li>
       </ul>
-      <h3></h3>
-      <p></p>
-      <button className="btn"></button>
+      <h3>Synopsis</h3>
+      <p>{movie.overview}</p>
+      <button className="btn" value="">
+        Ajouter aux coups de coeur
+      </button>
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,7 +1,37 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "./Card.js";
 
 const Movies = () => {
-  return <div className="result"></div>;
+  const [dataMovies, setDataMovies] = useState([]);
+
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/movie/popular?language=fr-FR",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2FlMDg0ZDVmYTRjNzMzZTZmYjJmMTY3Yzc2NTRjYiIsIm5iZiI6MTc2OTUwNzgwOC42NzI5OTk5LCJzdWIiOiI2OTc4OGJlMDBhNDI3ZDNhMjAxYTFhZjYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.3GIg1WOUtjvwHPplKz5WPBghwQYSSNcHaEo9Kilc0DE",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then((res) => {
+        setDataMovies(res.data.results);
+        console.log(res.data.results);
+      })
+      .catch((err) => console.error(err.data));
+  }, []);
+
+  return (
+    <div className="result">
+      {dataMovies.map((movie) => {
+        return <Card key={movie.id} movie={movie} />;
+      })}
+    </div>
+  );
 };
 
 export default Movies;
